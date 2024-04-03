47754019f516f09b2890e03f33dd6485d9b43d0e
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 12:19:19 +0300

    commit

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ mongodb.initDb((err) => {
     } 
   });
 
-  // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
